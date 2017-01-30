dcb93f1608d63a960e26230e29f3ab3f18ebbd26
Alexis Noel <noel.a@hotmail.fr>
2017-01-30 16:19:11 +0100

    Employes

--- employés.h
+++ employés.h
@@ -6,23 +6,22 @@ using namespace std;
 class Employes
 {
 public:
-    
+
     Employes();
-    
+
     void setName(string value);
     string getName() const;
-    
+
     void setPrenom(string value);
     string getPrenom() const;
-    
+
     void setAge(int value);
     int getAge() const;
-    
-    void setSalaireInitial(int value);
-    int getSalaireInitial() const;
-    
+
+    void SalaireInitial(int base);
+
     void affiche();
-    
+
 private:
     string m_name;
     string m_prenom;
